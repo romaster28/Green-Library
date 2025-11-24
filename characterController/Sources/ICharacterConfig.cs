89a4dc41e2b3efb81e0a0da784b2737ffabe4a09
romaster28 <sony.ba6@gmail.com>
2025-11-24 09:35:44 +0300

    First commit

---
+++ characterController/Sources/ICharacterConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public interface ICharacterConfig
+{
+    float MoveSpeed { get; }
+    float Acceleration { get; }
+    float AirControl { get; }
+    float JumpHeight { get; }
+    float Gravity { get; }
+    float GroundCheckRadius { get; }
+    LayerMask Ground { get; }
+    float MaxSlopeAngle { get; }
+}
